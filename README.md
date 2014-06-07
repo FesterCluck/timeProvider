6ec1f2e8b1351879ad110c2242e5cf7fa5cd6dcb
Stephen Kraushaar <skraushaar@gmail.com>
2014-06-07 06:52:54 -0500

    Added timeProvider and README changes.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 timeProvider
 ============
+
+
+timeProvider is a module which, when activated, will track date/time as the default Date() object does, but with a user-supplied date.
+This means it can be used in testing scenarios by simply requiring the module, then calling timeProvider.activate("Custom Date Here").
+Get the provided date through timeProvider.getDate(), the original set date through timeProvider.getStartDate()
+
+Additionally, this time provider allows for accelerating the clock. The second parameter on activate is a multiplier. There is also
+timeProvider.setSpeed(x). Valid numbers are 0 > x <= 1000. Get the current speed through timeProvider.getSpeed().
+
+The third parameter on activate is a tickFunction. Each time the clock ticks, the function is called. Additional functions can be
+added and removed via timeProvider.addTickFunction("name", function(){}) and timeProvider.removeTickFunction("name").
+
---
+++ timeProvider.js
@@ -0,0 +1,91 @@
+define(function() {
+	var TimeProviderFactory = function(objDate, objMultiplier, intervalFunc) {
+		if(!(objDate instanceof Date))
+			objDate = new Date(objDate);
+			
+		var testDate = !isNaN(objDate.getTime()) ? objDate : new Date();
+		var dateDiff = (new Date()).getTime() - testDate.getTime();
+		var accelDate = testDate;
+		var speed = parseInt(objMultiplier,10) || 1;
+		speed = speed > 1000 ? 1000 : speed;
+		speed = speed <= 0 ? 1 : speed;
+		
+		function fauxDate() {
+			var currentDate = new Date();
+			var sinceAccel = (currentDate.getTime()-dateDiff) - accelDate.getTime();
+			return new Date(accelDate.getTime() + (sinceAccel*speed));
+		}
+		var intVal = 1000/objMultiplier;
+		
+		var tickFunctions = [];
+		var tickFunctionNames = [];
+		
+		
+		var timeInterval = false;
+		if(intervalFunc && typeof(intervalFunc)==="function") {
+			tickFunctions._main = intervalFunc;
+			tickFunctionNames.push("_main");
+		}
+		
+		var intervalFuncs = function() {
+			for(var i = 0; i<tickFunctionNames.length; i++) {
+				tickFunctions[tickFunctionNames[i]]();
+			}
+		};
+		
+		timeInterval = window.setInterval(intervalFuncs, intVal);
+		
+		return {
+			getDate: function() {
+				return fauxDate();
+			},
+			
+			setSpeed: function(multiplier) {
+				multiplier = parseInt(multiplier,10) || 1;
+				multiplier = multiplier > 1000 ? 1000 : multiplier;
+				multiplier = multiplier <= 0 ? 1 : multiplier;
+				intVal = 1000/multiplier;
+				accelDate = new Date(fauxDate());
+				dateDiff = (new Date()).getTime() - accelDate.getTime();
+				speed = multiplier;
+				if(timeInterval) {
+				clearInterval(timeInterval);
+				timeInterval = window.setInterval(intervalFuncs, intVal);
+				}
+			},
+			getStartDate: function() {
+				return testDate;
+			},
+			addTickFunction: function(name, func) {
+				tickFunctions[name] = func;
+				tickFunctionNames.push(name);
+			},
+			removeTickFunction: function(name) {
+				clearInterval(timeInterval);
+				tickFunctionNames.splice(tickFunctionNames.indexOf(name),1);
+				delete tickFunctions[name];
+				timeInterval = window.setInterval(intervalFuncs, intVal);
+			},
+			getSpeed: function() {
+				return speed;
+			}
+		};
+	};
+
+	var timeProvider = {};
+	function activate(setDate, speed, func) {
+		timeProvider = new TimeProviderFactory(setDate,speed,func);
+	}
+	
+	return {
+		activate : function(setDate, speed, func) { activate(setDate, speed, func); },
+		getDate : function() { return timeProvider.getDate(); },
+		setSpeed: function(speed) { timeProvider.setSpeed(speed); },
+		getStartDate: function() { return timeProvider.getStartDate(); },
+		addTickFunction: function(name, func) { timeProvider.addTickFunction(name, func); },
+		removeTickFunction: function(name) { timeProvider.removeTickFunction(name); },
+		getSpeed: function() { timeProvider.getSpeed(); }
+	};
+});
+
+
